b3bc574c1f6b8cc13e0d0e960eb06f73c82ae043
Matei Pop <mapop@itu.dk>
2025-11-27 11:19:04 +0100

    ci:  correcting the order of the functions

---
+++ 99Problems/Module1/data/6_7_8_9_clean_separate.py
@@ -0,0 +1,50 @@
+#Cleaning
+import numpy as np
+
+data["lead_indicator"].replace("", np.nan, inplace=True)
+data["lead_id"].replace("", np.nan, inplace=True)
+data["customer_code"].replace("", np.nan, inplace=True)
+
+data = data.dropna(axis=0, subset=["lead_indicator"])
+data = data.dropna(axis=0, subset=["lead_id"])
+
+data = data[data.source == "signup"]
+result=data.lead_indicator.value_counts(normalize = True)
+
+print("Target value counter")
+for val, n in zip(result.index, result):
+    print(val, ": ", n)
+
+#Create and Separate
+vars = [
+    "lead_id", "lead_indicator", "customer_group", "onboarding", "source", "customer_code"
+]
+
+for col in vars:
+    data[col] = data[col].astype("object")
+    print(f"Changed {col} to object type")
+
+cont_vars = data.loc[:, ((data.dtypes=="float64")|(data.dtypes=="int64"))]
+cat_vars = data.loc[:, (data.dtypes=="object")]
+
+print("\nContinuous columns: \n")
+pprint(list(cont_vars.columns), indent=4)
+print("\n Categorical columns: \n")
+pprint(list(cat_vars.columns), indent=4)
+
+#Outliers
+cont_vars = cont_vars.apply(lambda x: x.clip(lower = (x.mean()-2*x.std()),
+                                             upper = (x.mean()+2*x.std())))
+outlier_summary = cont_vars.apply(describe_numeric_col).T
+outlier_summary.to_csv('./artifacts/outlier_summary.csv')
+outlier_summary
+cat_missing_impute = cat_vars.mode(numeric_only=False, dropna=True)
+cat_missing_impute.to_csv("./artifacts/cat_missing_impute.csv")
+
+#Impute Data
+cont_vars = cont_vars.apply(impute_missing_values)
+cont_vars.apply(describe_numeric_col).T
+cat_vars.loc[cat_vars['customer_code'].isna(),'customer_code'] = 'None'
+cat_vars = cat_vars.apply(impute_missing_values)
+cat_vars.apply(lambda x: pd.Series([x.count(), x.isnull().sum()], index = ['Count', 'Missing'])).T
+
